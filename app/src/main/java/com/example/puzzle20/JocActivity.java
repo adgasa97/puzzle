1370f136c79315d4f3413d46b6a3846a8b82c546
Ivan <ivanraid@gmail.com>
2020-02-23 16:44:05 +0100

    puzzle operativo, foto operativa

--- app/src/main/java/com/example/puzzle20/JocActivity.java
+++ app/src/main/java/com/example/puzzle20/JocActivity.java
@@ -5,21 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class JocActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap = null;
+    private PuzzleBoardView boardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joc);
 
+
+        // This code programmatically adds the PuzzleBoardView to the UI.
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.puzzle_container);
+        boardView = new PuzzleBoardView(this);
+        // Some setup of the view.
+        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        container.addView(boardView);
+
+
         //inicializar la toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            boardView.initialize(imageBitmap);
+        }
+    }
+
+    public void shuffleImage(View view) {
+        boardView.shuffle();
+    }
+
+    public void solve(View view) {
+        boardView.solve();
+
     }
 
     @Override
